88a3bcd8fba013623ce13a42a250e891e3cd5661
Giovanni Molina <gi.molina@profesor.duoc.cl>
2020-09-23 14:22:27 -0300

    Metodo Get All Comic
    Falta realizar iteracion

--- MarvelAPI/MarvelAPI/Connections/ComicAzure.cs
+++ MarvelAPI/MarvelAPI/Connections/ComicAzure.cs
@@ -1,7 +1,10 @@
 ﻿using MarvelAPI.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace MarvelAPI.Connections
@@ -10,5 +13,38 @@ namespace MarvelAPI.Connections
     {
         static string connectionString = @"Server=DESKTOP-S6JN4IA\SQLEXPRESS;Database=Marvel;Trusted_Connection=True;";
         public static List<Comic> listComic;
+
+        //Get all comics
+        public static List<Comic> getAllComics()
+        {
+            var dt = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(null, connection);
+                command.CommandText = "select * from Comic";
+
+                connection.Open();
+                var DataAdapter = new SqlDataAdapter(command);
+                DataAdapter.Fill(dt);
+
+                listComic = new List<Comic>();
+                Comic comic = new Comic();
+
+                comic.Nombre = dt.Rows[0]["Nombre"].ToString();
+                comic.idComic = int.Parse(dt.Rows[0]["idComic"].ToString());
+                comic.Autor = dt.Rows[0]["Autor"].ToString();
+                comic.Descrip = dt.Rows[0]["Descrip"].ToString();
+                //comic.FechPublic = dt.Rows[0]["FechPublic"].ToString();
+                comic.Rate = Convert.ToInt32(dt.Rows[0]["Rate"].ToString());
+
+                listComic.Add(comic);
+
+            }
+
+
+            return listComic;
+        }
+
     }
 }
---
+++ MarvelAPI/MarvelAPI/Controllers/ComicController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MarvelAPI.Connections;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MarvelAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComicController : ControllerBase
+    {
+        // GET: api/comic/all
+        [HttpGet("all")]
+        public JsonResult GetAllComic()
+        {
+            var list = ComicAzure.getAllComics();
+            return new JsonResult(list);
+        }
+        
+
+        // GET api/<ComicController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ComicController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ComicController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ComicController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
