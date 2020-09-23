cea38c32632b1652dccf2c5389b4fa7c5fa1b827
Giovanni Molina <gi.molina@profesor.duoc.cl>
2020-09-23 13:09:25 -0300

    first commit

---
+++ MarvelAPI/MarvelAPI/Connections/ComicAzure.cs
@@ -0,0 +1,14 @@
+﻿using MarvelAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarvelAPI.Connections
+{
+    public class ComicAzure
+    {
+        static string connectionString = @"Server=DESKTOP-S6JN4IA\SQLEXPRESS;Database=Marvel;Trusted_Connection=True;";
+        public static List<Comic> listComic;
+    }
+}
---
+++ MarvelAPI/MarvelAPI/Models/Comic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarvelAPI.Models
+{
+    public class Comic
+    {
+        public int idComic { get; set; }
+        public string Nombre { get; set; }
+        public string Autor { get; set; }
+        public string Descrip { get; set; }
+        public int Rate { get; set; }
+        public DateTime FechPublic { get; set; }
+
+    }
+}
